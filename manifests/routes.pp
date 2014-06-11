eb5cea898edf8f6cf2b1920906e9eea572b0585c
Damian Lukowski <damian.lukowski@credativ.de>
2014-06-11 11:55:11 +0200

    Remove files when the route definition vanishes.

--- manifests/routes.pp
+++ manifests/routes.pp
@@ -1,22 +1,26 @@
 class network::routes (
     $routes,
     ) {
-    if $routes != undef {
 
-         file { "/etc/network/ip-routes":
-          ensure  => file,
-          owner   => 'root',
-          group   => 'root',
-          mode    => '0644',
-          content => $routes,
-         }
-
-         file { "/etc/network/if-up.d/ip-routes":
-          ensure  => file,
-          owner   => 'root',
-          group   => 'root',
-          mode    => '0700',
-          source  => "puppet:///modules/${module_name}/ip-routes",
-        }
+    if $routes == undef {
+     $ensure = 'absent'
+    } else {
+     $ensure = 'file'
     }
+
+    file { "/etc/network/ip-routes":
+     ensure  => $ensure,
+     owner   => 'root',
+     group   => 'root',
+     mode    => '0644',
+     content => $routes,
+    }
+
+    file { "/etc/network/if-up.d/ip-routes":
+     ensure  => $ensure,
+     owner   => 'root',
+     group   => 'root',
+     mode    => '0700',
+     source  => "puppet:///modules/${module_name}/ip-routes",
+   }
 }
